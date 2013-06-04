827d5492808547c76788872fc4fb008fd26bce02
Noel Curtis <noelcurtis@gmail.com>
2013-06-03 22:04:34 -0400

    Updated...

--- app/controllers/GiftLists.scala
+++ app/controllers/GiftLists.scala
@@ -4,12 +4,12 @@ import play.api.mvc.{Action, Controller}
 import models.{User}
 import views.html
 import play.Logger
+import play.api.data.Form
+import play.api.data.Forms._
+import scala.Some
 
 object GiftLists extends Controller with Secured{
 
-  /**
-   * Display the dashboard.
-   */
   def index = IsAuthenticated { email => _ =>
     val user = User.find(email)
     user match {
@@ -17,5 +17,28 @@ object GiftLists extends Controller with Secured{
       case None => Logger.error("No user!"); Redirect(routes.Application.index).withNewSession
     }
   }
+
+  def create = IsAuthenticated{ email => _ =>
+    val user = User.find(email)
+    user match {
+      case Some(user) => Ok(html.gift_lists.create())
+      case None => Logger.error("No user!"); Redirect(routes.Application.index).withNewSession
+    }
+  }
+
+  val createForm = Form(
+    tuple(
+      "name" -> nonEmptyText,
+      "dueDate" -> text,
+      "notes" -> text
+    )
+  )
+
+//  /**
+//   * Handle create account submission.
+//   */
+//  def handlecreate = Action { implicit request =>
+//
+//  }
 
 }
