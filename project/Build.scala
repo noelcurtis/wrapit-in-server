fbea8aa1e4ad63c3d6af6687ce63a7b381b59278
Noel Curtis <noelcurtis@gmail.com>
2013-06-14 13:24:30 -0400

    Updated..

--- project/Build.scala
+++ project/Build.scala
@@ -12,12 +12,14 @@ object ApplicationBuild extends Build {
     jdbc,
     anorm,
     "postgresql" % "postgresql" % "9.1-901-1.jdbc4",
-    "org.jsoup" % "jsoup" % "1.7.2"
+    "org.jsoup" % "jsoup" % "1.7.2",
+    "nl.rhinofly" %% "api-s3" % "2.6.1"
   )
 
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
     // Add your own project settings here      
+    resolvers += "Rhinofly Internal Repository" at "http://maven-repository.rhinofly.net:8081/artifactory/libs-release-local"
   )
 
 }
