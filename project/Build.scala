3529e5784214e638bd90e6f49bb706f66fef0d96
Noel Curtis <noelcurtis@gmail.com>
2013-06-15 14:00:39 -0400

    Updated css fixes #1

--- project/Build.scala
+++ project/Build.scala
@@ -13,7 +13,8 @@ object ApplicationBuild extends Build {
     anorm,
     "postgresql" % "postgresql" % "9.1-901-1.jdbc4",
     "org.jsoup" % "jsoup" % "1.7.2",
-    "nl.rhinofly" %% "api-s3" % "2.6.1"
+    "nl.rhinofly" %% "api-s3" % "2.6.1",
+    "org.im4java" % "im4java" % "1.4.0"
   )
 
 
