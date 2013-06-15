06a9359e66863934ef5baa36d32ac29a78d38620
Noel Curtis <noelcurtis@gmail.com>
2013-06-15 15:29:35 -0400

    Updated prod config.

--- project/Build.scala
+++ project/Build.scala
@@ -4,7 +4,7 @@ import play.Project._
 
 object ApplicationBuild extends Build {
 
-  val appName         = "Gift-App-Server"
+  val appName         = "wrapit-in-server"
   val appVersion      = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
